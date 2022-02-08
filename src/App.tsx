ea61e2da333716958a4f6d816da1fd8b7a1af899
berlysia <berlysia@gmail.com>
2022-02-09 04:39:49 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,148 @@
+import React, { useEffect, useMemo, useState } from "react";
+
+import {
+  FlexibleWidthXYPlot,
+  XAxis,
+  YAxis,
+  HorizontalGridLines,
+} from "react-vis";
+import Candlestick from "./CandlestickSeries";
+
+type Data = {
+  name?: string | number;
+  min: number;
+  lowerQuartile: number;
+  median: number;
+  upperQuartile: number;
+  max: number;
+  average: number;
+  mark?: number;
+  ok?: number;
+};
+
+function useTransform(data: string) {
+  return useMemo(() => {
+    const [head, ...tail] = data
+      .trim()
+      .split("\n")
+      .map((row) => row.split(/\s+/));
+
+    const series: Data[] = tail.map(
+      (row) =>
+        Object.fromEntries(row.map((x, i) => [head[i], parseFloat(x)])) as Data
+    );
+    return series.map((v, i) => ({
+      x: i,
+      y: v.mark || v.average,
+      ...v,
+      size: 500,
+    }));
+  }, [data]);
+}
+
+function genBoldTicks(min: number, max: number) {
+  const result: number[] = [];
+  const minlog10 = Math.floor(Math.log10(min));
+  const maxlog10 = Math.floor(Math.log10(max));
+  const maxunit = 10 ** maxlog10;
+  const unit = 10 ** (maxlog10 - 1);
+  const x5 = 5 * unit;
+  if (min < x5) {
+    result.push(x5);
+  }
+  if (minlog10 < maxlog10) {
+    result.push(maxunit);
+  }
+  for (let cur = maxunit + x5; cur < max; cur += x5) {
+    result.push(cur);
+  }
+  return result;
+}
+function genThinTicks(_min: number, max: number) {
+  const result: number[] = [];
+  const maxlog10 = Math.floor(Math.log10(max));
+  const unit = 10 ** (maxlog10 - 1);
+  for (let i = 1; i * unit < max; i += 1) {
+    if (i % 5) result.push(i * unit);
+  }
+  return result;
+}
+
+export default function App() {
+  const [dataStr, setData] = useState("");
+  const [yMin, setYMin] = useState(0);
+  const [yMax, setYMax] = useState(100);
+  const seed = useTransform(dataStr);
+  useEffect(() => {
+    if (seed.length > 0) {
+      setYMin(Math.min(...seed.map((x) => x.min)) * 0.8);
+      setYMax(Math.max(...seed.map((x) => x.max)) * 1.2);
+    }
+  }, [seed]);
+  return (
+    <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+      <div>
+        <p>
+          format: multispace separated value
+          <br />
+          header row must be values listed below(unordered, parseFloat will be
+          applied):
+          <pre>
+            name min lowerQuartile median upperQuartile max average mark ok
+          </pre>
+        </p>
+        <textarea
+          style={{ width: 640, height: "4em" }}
+          value={dataStr}
+          onChange={(e) => setData(e.currentTarget.value)}
+          placeholder={`name min lowerQuartile median upperQuartile max average mark ok
+foo 0 20 40 60 80 50 75 1`}
+        ></textarea>
+        <label>
+          yMin
+          <input
+            type="number"
+            value={yMin}
+            onChange={(e) => setYMin(e.currentTarget.valueAsNumber || 0)}
+          ></input>
+        </label>
+        <label>
+          yMax
+          <input
+            type="number"
+            value={yMax}
+            onChange={(e) => setYMax(e.currentTarget.valueAsNumber || 0)}
+          ></input>
+        </label>
+      </div>
+      <div style={{ position: "relative", flexGrow: 1 }}>
+        <FlexibleWidthXYPlot
+          animation
+          yDomain={[yMin, yMax]}
+          xDomain={[0, seed.length]}
+          height={800}
+        >
+          <XAxis tickFormat={(x: number) => seed[x]?.name ?? ""} left={80} />
+          <YAxis />
+          <HorizontalGridLines
+            style={{ stroke: "#ddd", strokeDasharray: "8 12" }}
+            tickValues={genThinTicks(yMin, yMax)}
+          />
+          <HorizontalGridLines
+            style={{ stroke: "#bbb", strokeDasharray: "4 4" }}
+            tickValues={genBoldTicks(yMin, yMax)}
+          />
+          <Candlestick
+            colorType="literal"
+            opacityType="literal"
+            stroke="#79C7E3"
+            fill="#FF9833"
+            markOkStroke="#ff2929"
+            markNgStroke="#2929ff"
+            data={seed}
+          />
+        </FlexibleWidthXYPlot>
+      </div>
+    </div>
+  );
+}
---
+++ src/CandlestickSeries.tsx
@@ -0,0 +1,115 @@
+import { AbstractSeries } from "react-vis";
+
+const predefinedClassName =
+  "rv-xy-plot__series rv-xy-plot__series--candlestick";
+
+class CandlestickSeries extends AbstractSeries {
+  render() {
+    const { className, data, marginLeft, marginTop } = this.props;
+    if (!data) {
+      return null;
+    }
+
+    const xFunctor = this._getAttributeFunctor("x");
+    const yFunctor = this._getAttributeFunctor("y");
+    const strokeFunctor =
+      this._getAttributeFunctor("stroke") || this._getAttributeFunctor("color");
+    const fillFunctor =
+      this._getAttributeFunctor("fill") || this._getAttributeFunctor("color");
+    const opacityFunctor = this._getAttributeFunctor("opacity");
+
+    const distance = Math.abs(xFunctor(data[1]) - xFunctor(data[0])) * 0.2;
+
+    return (
+      <g
+        className={`${predefinedClassName} ${className}`}
+        transform={`translate(${marginLeft},${marginTop})`}
+      >
+        {data.map((d: any, i: number) => {
+          const xTrans = xFunctor(d);
+          const yHigh = yFunctor({ ...d, y: d.max });
+          const yOpen = yFunctor({ ...d, y: d.upperQuartile });
+          const yClose = yFunctor({ ...d, y: d.lowerQuartile });
+          const yLow = yFunctor({ ...d, y: d.min });
+          const yMark = yFunctor({ ...d, y: d.mark });
+          const yAverage = yFunctor({ ...d, y: d.average });
+          const yMedian = yFunctor({ ...d, y: d.median });
+          const ok = d.ok ?? 0;
+
+          const lineAttrs = {
+            stroke: strokeFunctor?.(d),
+          };
+          const markOkAttrs = {
+            stroke: this.props.markOkStroke || strokeFunctor?.(d),
+          };
+          const markNgAttrs = {
+            stroke: this.props.markNgStroke || strokeFunctor?.(d),
+          };
+
+          const xWidth = distance * 2;
+          return (
+            <g
+              key={i}
+              transform={`translate(${xTrans + xWidth})`}
+              opacity={opacityFunctor ? opacityFunctor(d) : 1}
+              onClick={(e) => this._valueClickHandler(d, e)}
+              onMouseOver={(e) => this._valueMouseOverHandler(d, e)}
+              onMouseOut={(e) => this._valueMouseOutHandler(d, e)}
+            >
+              <line
+                x1={-xWidth}
+                x2={xWidth}
+                y1={yHigh}
+                y2={yHigh}
+                {...lineAttrs}
+              />
+              <line x1={0} x2={0} y1={yHigh} y2={yLow} {...lineAttrs} />
+              <line
+                x1={-xWidth}
+                x2={xWidth}
+                y1={yLow}
+                y2={yLow}
+                {...lineAttrs}
+              />
+              <rect
+                x={-xWidth}
+                width={Math.max(xWidth * 2, 0)}
+                y={yOpen}
+                height={Math.abs(yOpen - yClose)}
+                fill={fillFunctor?.(d)}
+              />
+              <line
+                x1={-xWidth}
+                x2={xWidth}
+                y1={yMark}
+                y2={yMark}
+                strokeWidth={4}
+                {...(ok > 0 ? markOkAttrs : ok < 0 ? markNgAttrs : lineAttrs)}
+              />
+              <line
+                x1={-xWidth * 1.2}
+                x2={xWidth * 1.2}
+                y1={yAverage}
+                y2={yAverage}
+                strokeDasharray="6 6"
+                {...lineAttrs}
+              />
+              <line
+                x1={-xWidth}
+                x2={xWidth}
+                y1={yMedian}
+                y2={yMedian}
+                strokeDasharray="6 2 6 2"
+                {...lineAttrs}
+              />
+            </g>
+          );
+        })}
+      </g>
+    );
+  }
+}
+
+CandlestickSeries.displayName = "CandlestickSeries";
+
+export default CandlestickSeries as any;
